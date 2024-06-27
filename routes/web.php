365f68045d5cf4533cacf8142c4c99758f0cf428
SerchAlex13 <sergio.rosales5262@alumnos.udg.mx>
2024-06-27 16:47:24 -0600

    feat: Ajustar rutas

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,12 @@
 <?php
 
-use Illuminate\Foundation\Application;
+use App\Http\Controllers\AlumnoController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+Route::get('/', [DashboardController::class, 'index']);
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
+    Route::resource('/alumnos', AlumnoController::class);
 });
