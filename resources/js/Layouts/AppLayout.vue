97437f56038008f308136d9b3e27c49524bc6f5d
SerchAlex13 <sergio.rosales5262@alumnos.udg.mx>
2024-06-27 16:46:55 -0600

    feat: Crear nav alumnos en header

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -52,6 +52,12 @@ const logout = () => {
                                     Dashboard
                                 </NavLink>
                             </div>
+
+                            <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                                <NavLink :href="route('alumnos.index')" :active="route().current('alumnos.*')">
+                                    Alumnos
+                                </NavLink>
+                            </div>
                         </div>
 
                         <div class="hidden sm:flex sm:items-center sm:ms-6">
